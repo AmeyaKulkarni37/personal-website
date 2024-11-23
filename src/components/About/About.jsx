7c1912689308df27aeec9c45ad3bffb0db26cb47
Ameya Kulkarni <ameya.kulkarni@duke.edu>
2024-11-23 16:47:22 -0600

    Added about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./About.css";
 import { NavLink } from "react-router-dom";
+import profile_img from "../../assets/headshot.jpg";
 
 const About = () => {
   return (
     <div className="about">
-      <img src="" alt="" />
+      <img src={profile_img} alt="Professional headshot of Ameya Kulkarni" />
       <div className="about-text">
         <h1>Ameya Kulkarni</h1>
         <h2>Computer Science and Mathematics Student at Duke University</h2>
