87b4c86e41ccc58c50a19f1b358e57a12dfb071d
Ameya Kulkarni <ameya.kulkarni@duke.edu>
2024-11-29 11:20:53 -0600

    Finished skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,33 +1,42 @@
+import React from "react";
 import "./Skills.css";
 import html from "../../assets/logos/html5.svg";
 import css from "../../assets/logos/css3.svg";
+import java from "../../assets/logos/java.svg";
 import js from "../../assets/logos/JS.svg";
 import python from "../../assets/logos/python.svg";
 import react from "../../assets/logos/react.svg";
-const logo_path = "src/assets/logos";
+import linux from "../../assets/logos/linux.svg";
+import nodejs from "../../assets/logos/nodejs.svg";
+import R from "../../assets/logos/R.svg";
+import TypeScript from "../../assets/logos/TypeScript.svg";
 
 const Skills = () => {
   const getSkillItems = () => {
     const skillItems = [
-      "css3.svg",
-      "html5.svg",
-      "java.svg",
-      "JS.svg",
-      "linux.svg",
-      "nodejs.svg",
-      "python.svg",
-      "R.svg",
-      "react.svg",
-      "TypeScript.svg",
+      [python, "Python"],
+      [java, "Java"],
+      [react, "React.js"],
+
+      [html, "HTML"],
+      [css, "CSS"],
+      [js, "JavaScript"],
+      [linux, "Linux"],
+      [nodejs, "Node.js"],
+
+      [R, "R"],
+
+      [TypeScript, "TypeScript"],
     ];
 
     return (
-      <ul>
-        {skillItems.map((img) => {
+      <ul className="skill-items">
+        {skillItems.map((imgArr) => {
           return (
-            <li>
-              <img src={`${logo_path}${img}`} alt={img.split(".")[0]} />
-              <p>{`${img}`}</p>
+            <li key={imgArr[1]} id={imgArr[1]}>
+              <img src={imgArr[0]} alt={imgArr[1]} />
+
+              <p>{imgArr[1]}</p>
             </li>
           );
         })}
